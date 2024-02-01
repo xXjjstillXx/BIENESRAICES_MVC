e5de9263d397f04f17eed46fa4308f84b980b868
Andres Jaramillo <xXjjatillXx@gmail.com>
2024-01-31 19:19:31 -0500

    Conexion a base de datos

--- index.js
+++ index.js
@@ -4,9 +4,18 @@
 //Forma nueva de importar paquetes
 import express from 'express';
 import usuarioRoutes from './routes/usuarioRoutes.js';
+import db from './config/db.js';
 
 //Crear app
 const app = express();
+
+//conexion a la base de datos 
+try{
+    await db.authenticate();
+    console.log('Conexion establecida a la base de datos')
+}catch(error){
+    console.log(error)
+}
 
 //habilitar pug
 app.set('view engine','pug')
